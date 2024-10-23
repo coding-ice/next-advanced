8164aa89b8b115b3c07c6214a9d8e44555113f4d
ice <1597834867@qq.com>
2024-10-23 17:02:03 +0800

    refactor: Update App component and add Hover component

--- native/hook/src/App.tsx
+++ native/hook/src/App.tsx
@@ -1,10 +1,11 @@
+import Hover from './components/Hover';
 import { Global } from './config/theme';
 
 function App() {
   return (
     <>
       <Global />
-      <div>App</div>
+      <Hover />
     </>
   );
 }
