805c542bcbe70cc36290ccc1bd23958c568625d6
ice <1597834867@qq.com>
2024-10-29 15:04:46 +0800

    feat: Add @floating-ui/react and @ianvs/prettier-plugin-sort-imports dependencies

--- native/popover/src/App.tsx
+++ native/popover/src/App.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+import { useClick, useDismiss, useFloating, useHover, useInteractions } from '@floating-ui/react';
+
 import { Global } from './config/theme';
+
+function Popover() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: 'right-end',
+  });
+
+  // const hover = useHover(context);
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss]);
+
+  return (
+    <div style={{ margin: 200 }}>
+      <button ref={refs.setReference} {...getReferenceProps()}>
+        hi
+      </button>
+      {isOpen && (
+        <div ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
+          iceice
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
       <Global />
-      <div>App</div>
+      <Popover />
     </>
   );
 }
