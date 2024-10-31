12bca9fe4bea5737f67f9442f87b91628ec60d6a
ice <1597834867@qq.com>
2024-10-31 22:26:54 +0800

    Refactor code and add new pages in 5.1Parallel_Routes directory

--- step1/1.Hello_Next/app/page.jsx
+++ step1/1.Hello_Next/app/page.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Page() {
   return (
     <React.Profiler id="hello">
-      {new Array(100000).fill(0).map(() => (
-        <h3>Hello Next.js!</h3>
-      ))}
+      <h3>Hello Next.js!</h3>
     </React.Profiler>
   );
 }
