bb0bbab8eef262685506ada09358c2fbec65946f
ice <1597834867@qq.com>
2025-08-21 19:48:26 +0800

    feat: Add delay to TeamPage component for improved user experience
    - Introduced a sleep function to delay rendering of the TeamPage component by 3 seconds.
    - Updated TeamPage to be an async function to accommodate the new delay feature.

--- step1/1.parallel_routes/app/@team/page.tsx
+++ step1/1.parallel_routes/app/@team/page.tsx
@@ -1,4 +1,7 @@
-const TeamPage = () => {
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const TeamPage = async () => {
+  await sleep(3000);
   return <div>TeamPage</div>;
 };
 
