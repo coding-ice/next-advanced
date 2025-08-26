22a6ff0e92a6f928eb45e823662391e23f794e9a
ice <1597834867@qq.com>
2025-08-26 22:54:13 +0800

    feat: Add API route for fetching post data by ID
    - Implemented a new GET endpoint in the posts API to retrieve specific post data based on the provided ID.
    - The endpoint extracts a query parameter to return a specific field from the fetched post data.

---
+++ step1/3.路由处理程序/app/api/posts/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: IParams) {
+  const { id } = await params;
+  const dataField = request.nextUrl.searchParams.get("dataField");
+
+  const data = await (
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  ).json();
+  return NextResponse.json({ [dataField]: data[dataField] });
+}
