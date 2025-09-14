a7cb2d173af4d6c389c296839d3239e0ba371835
ice <1597834867@qq.com>
2025-09-14 22:05:58 +0800

    feat: Update HomePage component for dynamic data fetching
    - Introduced a caching mechanism to prevent React's request memory, ensuring multiple requests trigger only one fetch.
    - Updated the HomePage and ImgCache components to utilize the new data fetching approach, enhancing performance and rendering.
    - Added a timestamp display to the HomePage for better user experience.

--- step1/7.cache_fetch_memo_data_cache/app/page.tsx
+++ step1/7.cache_fetch_memo_data_cache/app/page.tsx
@@ -1,21 +1,21 @@
-// 改为动态渲染，取消 nextjs 的数据缓存
-export const dynamic = "force-dynamic";
+import { useEffect, useState } from "react";
 
 async function getData() {
+  // 取消 react 的请求记忆，在服务端 React 组件树中，多次请求只会发起一次请求
   const { signal } = new AbortController();
-  // 取消 react 的请求记忆
   const result = await fetch("https://api.thecatapi.com/v1/images/search", {
     signal,
+    cache: "no-store",
   });
   return await result.json();
 }
 
 const HomePage = async () => {
-  const res = await getData();
-  const data = res[0];
+  const [data] = await getData();
 
   return (
     <div>
+      <h2>{new Date().toLocaleTimeString()}</h2>
       <h3>{data.id}</h3>
       <img style={{ width: 300 }} src={data.url} alt="cat" />
       <hr />
@@ -25,8 +25,7 @@ const HomePage = async () => {
 };
 
 const ImgCache = async () => {
-  const res = await getData();
-  const data = res[0];
+  const [data] = await getData();
 
   return (
     <>
