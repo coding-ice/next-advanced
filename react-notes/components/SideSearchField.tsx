58a6c7c4ac39dd06693fd79e33461928c32daa12
ice <1597834867@qq.com>
2025-10-02 21:33:57 +0800

    feat: Update SideSearchField component with input and button for enhanced search functionality
    - Redesigned the SideSearchField component to include a search input and button, improving user interaction.
    - Utilized Tailwind CSS for styling, ensuring a modern and responsive layout.

--- react-notes/components/SideSearchField.tsx
+++ react-notes/components/SideSearchField.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function SideSearchField() {
-  return <div className="">SideSearchField</div>;
+  return (
+    <div className="flex gap-4">
+      <input className="flex-1 w-full h-8 rounded-full border border-black/10 px-2" />
+      <button className="bg-[#037dba] text-white h-8 px-3 rounded-2xl cursor-pointer hover:bg-[#0396df]">
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SideSearchField;
