c2a450cf8c483be0a8c38973767eceb5428bb608
ice <1597834867@qq.com>
2025-10-03 10:42:35 +0800

    feat: Enhance NoteItem and NoteItemContent components with expandable content feature
    - Added an expandable content feature to NoteItem, allowing users to view additional note details.
    - Updated NoteItemContent to include a toggle button for expanding and collapsing content, improving user interaction.
    - Modified SVG icons for chevron indicators to use currentColor for better styling flexibility.
    - Introduced new nursery linter rules in biome.json for improved code quality.

--- react-notes/components/NoteItemContent.tsx
+++ react-notes/components/NoteItemContent.tsx
@@ -2,26 +2,48 @@
 
 import { usePathname } from "next/navigation";
 import type React from "react";
+import { useState } from "react";
+import ChevronDown from "@/assets/svg/chevron-down.svg";
+import ChevronUp from "@/assets/svg/chevron-up.svg";
+import Link from "next/link";
 
 interface NoteItemContentProps {
-	id: string;
-	children: React.ReactNode;
+  id: string;
+  children: React.ReactNode;
+  expendedNode?: React.ReactNode;
 }
 
-const NoteItemContent = ({ id, children }: NoteItemContentProps) => {
-	const pathname = usePathname();
-	const selectedId = pathname.split("/").pop();
-	const isActive = selectedId === id;
+const NoteItemContent = ({
+  id,
+  children,
+  expendedNode,
+}: NoteItemContentProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-	return (
-		<li
-			className={`flex flex-col gap-1 cursor-pointer p-4 rounded-md ${
-				isActive ? "bg-gray-100 border-[#037dba]" : ""
-			}`}
-		>
-			{children}
-		</li>
-	);
+  const pathname = usePathname();
+  const selectedId = pathname.split("/").pop();
+  const isActive = selectedId === id;
+
+  return (
+    <li>
+      <Link
+        className={`relative flex flex-col gap-1 cursor-pointer p-4 rounded-md ${
+          isActive ? "bg-gray-100 border-[#037dba]" : ""
+        }`}
+        href={`/note/${id}`}
+      >
+        {children}
+        {expendedNode && isExpanded && expendedNode}
+        <button
+          className="flex justify-center items-center absolute right-4 top-4 rounded-full w-6 h-6 bg-gray-300 hover:bg-gray-400 hover:text-white"
+          onClick={() => setIsExpanded(!isExpanded)}
+          type="button"
+        >
+          {isExpanded ? <ChevronUp /> : <ChevronDown />}
+        </button>
+      </Link>
+    </li>
+  );
 };
 
 export default NoteItemContent;
