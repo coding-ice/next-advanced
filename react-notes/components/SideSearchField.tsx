bcbc5fca2cbf5fd422fea31fec413727042a854d
ice <1597834867@qq.com>
2025-10-05 14:58:24 +0800

    feat: Enhance SideSearchField with search functionality
    - Added search handling in SideSearchField component to update URL parameters based on user input.
    - Implemented useTransition for smoother state updates during search operations.
    - Integrated usePathname and useRouter from Next.js for improved navigation and state management.

--- react-notes/components/SideSearchField.tsx
+++ react-notes/components/SideSearchField.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useTransition } from "react";
 import EditButton from "./EditButton";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function SideSearchField() {
+	const { replace } = useRouter();
+	const pathname = usePathname();
+	const [isPending, startTransition] = useTransition();
+
+	const handleSearch = (term: string) => {
+		const params = new URLSearchParams(location.search);
+		if (term) {
+			params.set("q", term);
+		} else {
+			params.delete("q");
+		}
+		startTransition(() => {
+			replace(`${pathname}?${params.toString()}`);
+		});
+	};
+
 	return (
 		<div className="flex gap-4">
-			<input className="flex-1 w-full h-8 rounded-full border border-black/10 px-2" />
+			<input
+				className="flex-1 w-full h-8 rounded-full border border-black/10 px-2"
+				onChange={(e) => handleSearch(e.target.value)}
+			/>
 			<EditButton>New</EditButton>
 		</div>
 	);
