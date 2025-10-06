e932192f0a95e71df1ce991e034c45698774f07c
ice <1597834867@qq.com>
2025-10-06 16:37:13 +0800

    feat: Add next-auth dependency and update layout with Header component
    - Added "next-auth" dependency in package.json for authentication support.
    - Updated pnpm-lock.yaml to include new dependency resolutions.
    - Refactored layout.tsx to integrate Header component for improved layout structure.

--- react-notes/app/layout.tsx
+++ react-notes/app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from "@/components/Sidebar";
 import { Toaster } from "sonner";
 
 import "./global.css";
+import Header from "@/components/Header";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
@@ -9,7 +10,10 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 			<body>
 				<div className="flex gap-4 w-screen h-screen">
 					<Sidebar />
-					<div className="flex-1">{children}</div>
+					<div className="flex-1">
+						<Header />
+						{children}
+					</div>
 					<Toaster />
 				</div>
 			</body>
